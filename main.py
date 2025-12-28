79241df9464dec7d0c5b198c5b55db6a10aa4b7c
yanuarcaturp <yanuarcaturp22@gmail.com>
2025-12-28 14:56:59 +0700

    fix railway startup final

--- main.py
+++ main.py
@@ -44,5 +44,5 @@ if __name__ == "__main__":
     import os
 
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
     
